e8be0748f02b00d5825ff786ebd04a99554758e5
filipandersson3 <60388955+filipandersson3@users.noreply.github.com>
2021-10-17 11:22:43 +0200

    feat: ASCII visualisering

--- src/MazeProgram/Main.java
+++ src/MazeProgram/Main.java
@@ -2,6 +2,7 @@ package MazeProgram;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.io.IOException;
 
@@ -15,8 +16,23 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        int width = mazeImg.getWidth();
+        int height = mazeImg.getHeight();
+        byte[] pixels = ((DataBufferByte) mazeImg.getRaster().getDataBuffer()).getData();
         System.out.println(mazeImg.getWidth());
         System.out.println(mazeImg.getHeight());
+        for (int i = 0; i < width*height; i++) {
+            if (pixels[i] == -1) {
+                System.out.print(" ");
+            }
+            if (pixels[i] == 0) {
+                System.out.print("█");
+            }
+            if ((i+1)%(width) == 0) {
+                System.out.println("");
+            }
+        }
+
 
     }
 }
